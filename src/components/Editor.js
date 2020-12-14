5d67f8a82de3cbcec8e1d5233ae47d7cb1a28866
shakirck <shakirck333@gmail.com>
2020-12-14 14:36:24 +0530

    edited default Template fetch

--- src/components/Editor.js
+++ src/components/Editor.js
@@ -1,30 +1,19 @@
 import React, { useEffect, useRef, useState } from "react";
-import { text } from "react-markdown/lib/renderers";
 import templateMD from "../utils/dilinger.md";
-
 export const Editor = ({ setstate, state }) => {
   const [editorState, seteditorState] = useState("");
   const [defaultState, setdefaultState] = useState("");
   const defaultValue = useRef(state);
   useEffect(() => {
-    const readTextFile = (file) => {
-      var rawFile = new XMLHttpRequest();
-      rawFile.open("GET", file);
-      rawFile.onreadystatechange = () => {
-        if (rawFile.readyState === 4) {
-          if (rawFile.status === 200 || rawFile.status == 0) {
-            var allText = rawFile.responseText;
-            console.log(allText);
-            setdefaultState(allText);
-            setstate(allText);
-          }
-        }
-      };
-      rawFile.send(null);
-    };
-    console.log(templateMD);
-    const data = readTextFile(templateMD);
+    readTextFile();
   }, []);
+  const readTextFile = async () => {
+    const res = await fetch(templateMD);
+    const data = await res.text();
+    console.log(data);
+    setdefaultState(data);
+    setstate(data);
+  };
   const handleEditorInput = (e) => {
     setstate(e.target.innerText);
   };
@@ -35,10 +24,7 @@ export const Editor = ({ setstate, state }) => {
     seteditorState(inputText);
     // console.log(textarr);
   };
-  const handleTextSplitting = () => {
-    const textarr = editorState.split("\n");
-    setstate(textarr);
-  };
+
   return (
     <pre
       className="editor-container container"
