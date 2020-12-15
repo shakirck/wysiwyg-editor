f55a06bb1173e4fe15d12867b3a05de26cf7ad3b
shakirck <shakirck333@gmail.com>
2020-12-15 20:32:01 +0530

    added showdown markdown support

--- src/components/PreviewBlock.js
+++ src/components/PreviewBlock.js
@@ -1,8 +1,27 @@
 import React from "react";
 import marked from "marked";
+import showdown from "showdown";
+// showdown.setFlavor("github");
 export const PreviewBlock = ({ block }) => {
+  const showdownOption = {
+    parseImgDimensions: true,
+    simplifiedAutoLink: true,
+    tables: true,
+    tasklists: true,
+    smoothLivePreview: true,
+    smartIndentationFix: true,
+    openLinksInNewWindow: true,
+    emoji: true,
+    prefixHeaderId: true,
+    ghCodeBlocks: true,
+    disableForced4SpacesIndentedSublists: true,
+  };
   const getMarkup = () => {
-    const markup = marked(block.text, { sanitize: true });
+    // const markup = marked(block.text, { sanitize: true });
+    const converter = new showdown.Converter(showdownOption);
+    var thisConverterSpecificOptions = converter.getOptions();
+    console.log("options ", thisConverterSpecificOptions);
+    const markup = converter.makeHtml(block.text);
     return { __html: markup };
   };
 
